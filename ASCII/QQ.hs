b6be45d9cb14cd0a961db07b3226cf6297e78bde
Chris Martin <ch.martin@gmail.com>
2019-12-02 02:19:19 -0700

    add doc to QQ

--- ASCII/QQ.hs
+++ ASCII/QQ.hs
@@ -9,6 +9,27 @@ import Language.Haskell.TH.Syntax (Exp, Lift (lift), Q)
 
 import qualified Control.Monad.Fail as Fail
 
+{- | Produces an expression representing a 'ASCII.String' value corresponding to the quasiquoted string.
+
+> >>> :set -XQuasiQuotes
+>
+> >>> import qualified ASCII
+>
+> >>> import ASCII.QQ
+>
+> >>> ASCII.unpack [ascii|Hi!|]
+> [CapitalLetterH,SmallLetterI,ExclamationMark]
+
+The expression fails to compile if the quasiquoted string contains characters that cannot be represented in the ASCII character set.
+
+> >>> [ascii|helloλ|]
+>
+> interactive>:4:8: error:
+>     • ascii cannot be used with non-ASCII characters
+>     • In the quasi-quotation: [ascii|helloλ|]
+
+-}
+
 ascii :: QuasiQuoter
 ascii =
   QuasiQuoter
