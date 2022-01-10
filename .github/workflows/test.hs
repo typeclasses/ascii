47427cc401531b10b963a133ebe774d57cd3784d
Chris Martin <ch.martin@gmail.com>
2022-01-09 22:19:42 -0700

    support ghc 9.2

--- .github/workflows/test.hs
+++ .github/workflows/test.hs
@@ -11,7 +11,7 @@ main =
 x .= Just y  = Just ("--constraint=" ++ x ++ "==" ++ y)
 x .= Nothing = Nothing
 
-data GHC = GHC_8_4 | GHC_8_6 | GHC_8_8 | GHC_8_10 | GHC_9_0
+data GHC = GHC_8_4 | GHC_8_6 | GHC_8_8 | GHC_8_10 | GHC_9_0 | GHC_9_2
 
 readGHC s = case s of
     "8.4"  -> GHC_8_4
@@ -19,6 +19,7 @@ readGHC s = case s of
     "8.8"  -> GHC_8_8
     "8.10" -> GHC_8_10
     "9.0"  -> GHC_9_0
+    "9.2"  -> GHC_9_2
 
 constraints ghc = catMaybes
     [ "base" .= case ghc of
@@ -27,6 +28,7 @@ constraints ghc = catMaybes
           GHC_8_8  -> Just "4.13.*"
           GHC_8_10 -> Just "4.14.*"
           GHC_9_0  -> Just "4.15.*"
+          GHC_9_2  -> Just "4.16.*"
     , "bytestring" .= case ghc of
           GHC_8_4  -> Just "0.10.*"
           GHC_8_8  -> Just "0.11.*"
@@ -42,6 +44,7 @@ constraints ghc = catMaybes
           GHC_8_8  -> Just "2.15.*"
           GHC_8_10 -> Just "2.16.*"
           GHC_9_0  -> Just "2.17.*"
+          GHC_9_2  -> Just "2.18.*"
     , "text" .= case ghc of
           GHC_8_4  -> Just "1.2.3.0"
           GHC_8_10 -> Just "1.2.4.*"
