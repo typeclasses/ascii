bc06bb6e29362cb81550406cf7af15b55c610fa1
Chris Martin <ch.martin@gmail.com>
2022-03-22 16:22:24 -0600

    switch ascii-group tests to hedgehog

--- ascii-group/test/Main.hs
+++ ascii-group/test/Main.hs
@@ -4,54 +4,61 @@ import ASCII.Group
 
 import ASCII.Char (Char (..), allCharacters)
 
-import Control.Applicative (Applicative (..))
 import Control.Monad (Monad (..))
-import Data.Bool (Bool (..), not)
-import Data.Eq (Eq ((==)))
+import Data.Bool (not)
 import Data.Foldable (all)
-import Data.Function (($), (.))
-import Data.Functor (Functor (..))
-import Data.List (filter, intercalate, length, map, null)
-import Data.Semigroup ((<>))
-import Numeric.Natural (Natural)
-import System.Exit (die)
-import System.IO (IO, putStrLn)
-import Text.Show (show)
+import Data.Function (($))
+import Data.List (filter, length)
+import System.IO (IO)
+import Control.Monad (when)
+import System.Exit (exitFailure)
+
+import Hedgehog (Property, assert, checkParallel, discover, property,
+                 withTests, (===))
 
 main :: IO ()
-main = dieIfFailures $ do
-    test 1 $ map charGroup [CapitalLetterA, EndOfTransmission] == [Printable, Control]
-    test 2 $ not $ inGroup Printable EndOfTransmission
-    test 3 $ inGroup Control EndOfTransmission
-    test 4 $ charGroup Space == Printable
-    test 5 $ charGroup HorizontalTab == Control
-    test 6 $ all (inGroup Printable) [CapitalLetterA, SmallLetterZ, Digit4, Tilde]
-    test 7 $ all (inGroup Control) [Null, Substitute, UnitSeparator, Delete]
-    test 8 $ length (filter (inGroup Control) allCharacters) == 33
-    test 9 $ length (filter (inGroup Printable) allCharacters) == 95
+main = checkParallel $$(discover) >>= \ok -> when (not ok) exitFailure
 
-dieIfFailures :: Failures a -> IO a
-dieIfFailures (Failures fs x) =
-    if null fs
-        then do putStrLn "💯"; return x
-        else die $ intercalate " " (map (("🔥" <> ) . show) fs)
+prop_letter :: Property
+prop_letter = withTests 1 $ property $
+    charGroup CapitalLetterA === Printable
 
-type TestNumber = Natural
+prop_control :: Property
+prop_control = withTests 1 $ property $
+    charGroup EndOfTransmission === Control
 
-test :: TestNumber -> Bool -> Failures ()
-test n t = Failures (if t then [] else [n]) ()
+prop_not_printable :: Property
+prop_not_printable = withTests 1 $ property $
+    assert $ not $ inGroup Printable EndOfTransmission
 
-data Failures a = Failures [TestNumber] a
+prop_is_control :: Property
+prop_is_control = withTests 1 $ property $
+    assert $ inGroup Control EndOfTransmission
 
-instance Functor Failures
-  where
-    fmap f (Failures a x) = Failures a (f x)
+-- It is perhaps surprising that space is considered a
+-- "printable" character, since it does not visibly appear.
+prop_space_is_printable :: Property
+prop_space_is_printable = withTests 1 $ property $
+    charGroup Space === Printable
 
-instance Applicative Failures
-  where
-    pure x = Failures [] x
-    Failures a f <*> Failures b x = Failures (a <> b) (f x)
+-- It is perhaps surprising that horizontal tab is not
+-- in the same category as space.
+prop_horizontal_tab_is_control :: Property
+prop_horizontal_tab_is_control = withTests 1 $ property $
+    charGroup HorizontalTab === Control
 
-instance Monad Failures
-  where
-    Failures a x >>= f = let Failures b y = f x in Failures (a <> b) y
+prop_various_printables :: Property
+prop_various_printables = withTests 1 $ property $
+    assert $ all (inGroup Printable) [CapitalLetterA, SmallLetterZ, Digit4, Tilde]
+
+prop_various_controls :: Property
+prop_various_controls = withTests 1 $ property $
+    assert $ all (inGroup Control) [Null, Substitute, UnitSeparator, Delete]
+
+prop_count_printables :: Property
+prop_count_printables = withTests 1 $ property $
+    length (filter (inGroup Printable) allCharacters) === 95
+
+prop_count_controls :: Property
+prop_count_controls = withTests 1 $ property $
+    length (filter (inGroup Control) allCharacters) === 33
