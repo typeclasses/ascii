5dc8c13756ce019d282641b200fd500f3c263be3
Chris Martin <ch.martin@gmail.com>
2022-03-22 15:24:24 -0600

    switch ascii-th tests over to hedgehog

--- ascii-th/test/Main.hs
+++ ascii-th/test/Main.hs
@@ -1,73 +1,109 @@
 module Main (main) where
 
-import ASCII.QuasiQuoters
-import ASCII.TemplateHaskell
+import ASCII.QuasiQuoters (char, string)
+import ASCII.TemplateHaskell (charExp, charListExp, charListPat, charPat,
+                              isCharExp, isCharPat)
 
 import ASCII.Char (Char (..))
 import ASCII.Refinement (ASCII, asciiUnsafe)
 import ASCII.Superset (toCharOrFail)
 
-import Control.Applicative (Applicative (..))
-import Control.Monad (Monad (..))
-import Data.Bool (Bool (..))
-import Data.Eq (Eq ((==)))
-import Data.Function ((.), ($))
-import Data.Functor (Functor (..))
-import Data.List (intercalate, map, null)
-import Data.Semigroup ((<>))
+import Control.Monad (Monad (..), when)
+import Data.Bool (not)
+import Data.Function (($))
 import Data.String (String)
 import Data.Text (Text)
 import Data.Word (Word8)
-import Numeric.Natural (Natural)
 import Prelude (Integer)
-import System.Exit (die)
-import System.IO (IO, putStrLn)
-import Text.Show (show)
+import System.Exit (exitFailure)
+import System.IO (IO)
 
 import qualified Data.ByteString.Builder as BS.Builder
 
+import Hedgehog (Property, checkParallel, discover, property, withTests, (===))
+
 main :: IO ()
-main = dieIfFailures $ do
-    test 1 $ ([char|e|] :: Char) == SmallLetterE
-    test 2 $ ([char|e|] :: Word8) == 101
-    test 3 $ (case Tilde of [char|@|] -> 1 :: Integer; [char|~|] -> 2; _ -> 3) == 2
-    test 4 $ ([string|Hello!|] :: [Char]) == [CapitalLetterH, SmallLetterE, SmallLetterL, SmallLetterL, SmallLetterO, ExclamationMark]
-    test 5 $ ([string|Hello!|] :: String) == "Hello!"
-    test 6 $ ([string|Hello!|] :: Text) == "Hello!"
-    test 7 $ (BS.Builder.toLazyByteString [string|Hello!|] == "Hello!")
-    test 8 $ (case [CapitalLetterH, SmallLetterI] of [string|Bye|] -> 1 :: Integer; [string|Hi|] -> 2; _ -> 3) == 2
-    test 9 $ $(toCharOrFail 'F' >>= charExp) == CapitalLetterF
-    test 10 $ $(toCharOrFail '\DEL' >>= charExp) == Delete
-    test 11 $ case SmallLetterS of { $(toCharOrFail 'r' >>= charPat) -> 1 :: Integer; $(toCharOrFail 's' >>= charPat) -> 2; _ -> 3 } == 2
-    test 12 $ $(charListExp [CapitalLetterH, SmallLetterI]) == [CapitalLetterH, SmallLetterI]
-    test 13 $ case [CapitalLetterH, SmallLetterI] of { $(charListPat [CapitalLetterH, SmallLetterA]) -> 1 :: Integer; $(charListPat [CapitalLetterH, SmallLetterI]) -> 2; _ -> 3 } == 2
-    test 14 $ ($(isCharExp CapitalLetterA) :: Char) == CapitalLetterA
-    test 15 $ ($(isCharExp CapitalLetterA) :: Word8) == 65
-    test 16 $ ($(isCharExp CapitalLetterA) :: ASCII Word8) == asciiUnsafe 65
-    test 17 $ case (66 :: Word8) of { $(isCharPat CapitalLetterA) -> 1 :: Integer; $(isCharPat CapitalLetterB) -> 2; _ -> 3 } == 2
+main = checkParallel $$(discover) >>= \ok -> when (not ok) exitFailure
 
-dieIfFailures :: Failures a -> IO a
-dieIfFailures (Failures fs x) =
-    if null fs
-        then do putStrLn "💯"; return x
-        else die $ intercalate " " (map (("🔥" <> ) . show) fs)
+prop_smallE :: Property
+prop_smallE = withTests 1 $ property $
+    ([char|e|] :: Char) === SmallLetterE
 
-type TestNumber = Natural
+prop_smallE_word8 :: Property
+prop_smallE_word8 = withTests 1 $ property $
+    ([char|e|] :: Word8) === 101
 
-test :: TestNumber -> Bool -> Failures ()
-test n t = Failures (if t then [] else [n]) ()
+prop_tilde_pattern :: Property
+prop_tilde_pattern = withTests 1 $ property $
+    2 === case Tilde of
+        [char|@|] -> 1 :: Integer
+        [char|~|] -> 2
+        _ -> 3
 
-data Failures a = Failures [TestNumber] a
+prop_hello_list :: Property
+prop_hello_list = withTests 1 $ property $
+    ([string|Hello!|] :: [Char]) === [CapitalLetterH, SmallLetterE, SmallLetterL, SmallLetterL, SmallLetterO, ExclamationMark]
 
-instance Functor Failures
-  where
-    fmap f (Failures a x) = Failures a (f x)
+prop_hello_string :: Property
+prop_hello_string = withTests 1 $ property $
+    ([string|Hello!|] :: String) === "Hello!"
 
-instance Applicative Failures
-  where
-    pure x = Failures [] x
-    Failures a f <*> Failures b x = Failures (a <> b) (f x)
+prop_hello_text :: Property
+prop_hello_text = withTests 1 $ property $
+    ([string|Hello!|] :: Text) === "Hello!"
 
-instance Monad Failures
-  where
-    Failures a x >>= f = let Failures b y = f x in Failures (a <> b) y
+prop_string_qq_expression :: Property
+prop_string_qq_expression = withTests 1 $ property $
+    BS.Builder.toLazyByteString [string|Hello!|] === "Hello!"
+
+prop_string_qq_pattern :: Property
+prop_string_qq_pattern = withTests 1 $ property $
+    2 === case [CapitalLetterH, SmallLetterI] of
+        [string|Bye|] -> 1 :: Integer
+        [string|Hi|] -> 2
+        _ -> 3
+
+prop_char_splice_letter :: Property
+prop_char_splice_letter = withTests 1 $ property $
+    $(toCharOrFail 'F' >>= charExp) === CapitalLetterF
+
+prop_char_splice_del :: Property
+prop_char_splice_del = withTests 1 $ property $
+    $(toCharOrFail '\DEL' >>= charExp) === Delete
+
+prop_char_splice_pattern :: Property
+prop_char_splice_pattern = withTests 1 $ property $
+    2 === case SmallLetterS of
+        $(toCharOrFail 'r' >>= charPat) -> 1 :: Integer
+        $(toCharOrFail 's' >>= charPat) -> 2
+        _ -> 3
+
+prop_char_list_splice :: Property
+prop_char_list_splice = withTests 1 $ property $
+    $(charListExp [CapitalLetterH, SmallLetterI]) === [CapitalLetterH, SmallLetterI]
+
+prop_char_list_splice_pattern :: Property
+prop_char_list_splice_pattern = withTests 1 $ property $
+    2 === case [CapitalLetterH, SmallLetterI] of
+        $(charListPat [CapitalLetterH, SmallLetterA]) -> 1 :: Integer
+        $(charListPat [CapitalLetterH, SmallLetterI]) -> 2
+        _ -> 3
+
+prop_polymorphic_char_splice :: Property
+prop_polymorphic_char_splice = withTests 1 $ property $
+    ($(isCharExp CapitalLetterA) :: Char) === CapitalLetterA
+
+prop_polymorphic_char_splice_word8 :: Property
+prop_polymorphic_char_splice_word8 = withTests 1 $ property $
+    ($(isCharExp CapitalLetterA) :: Word8) === 65
+
+prop_polymorphic_char_splice_ascii_word8 :: Property
+prop_polymorphic_char_splice_ascii_word8 = withTests 1 $ property $
+    ($(isCharExp CapitalLetterA) :: ASCII Word8) === asciiUnsafe 65
+
+prop_polymorphic_char_splice_pattern :: Property
+prop_polymorphic_char_splice_pattern = withTests 1 $ property $
+    2 === case (66 :: Word8) of
+        $(isCharPat CapitalLetterA) -> 1 :: Integer
+        $(isCharPat CapitalLetterB) -> 2
+        _ -> 3
