fffc27bec678513baf05bf279895446c1dc8f35d
Chris Martin <ch.martin@gmail.com>
2023-01-03 15:15:32 -0700

    fidget with ASCII module doc

--- ascii/library/ASCII.hs
+++ ascii/library/ASCII.hs
@@ -1,15 +1,31 @@
-{- |
+{-| The __American Standard Code for Information Interchange__ (ASCII) comprises
+a set of 128 characters, each represented by 7 bits. 33 of these characters are
+/'Control' codes/; a few of these are still in use, but most are obsolete relics
+of the early days of computing. The other 95 are /'Printable' characters/ such
+as letters and numbers, mostly corresponding to the keys on an American English
+keyboard.
 
-The __American Standard Code for Information Interchange__ (ASCII) comprises a set of 128 characters, each represented by 7 bits. 33 of these characters are /'Control' codes/; a few of these are still in use, but most are obsolete relics of the early days of computing. The other 95 are /'Printable' characters/ such as letters and numbers, mostly corresponding to the keys on an American English keyboard.
+Nowadays instead of ASCII we typically work with text using an encoding such as
+UTF-8 that can represent the entire Unicode character set, which includes over a
+hundred thousand characters and is not limited to the symbols of any particular
+writing system or culture. However, ASCII is still relevant to network
+protocols; for example, we can see it in the specification of
+<https://tools.ietf.org/html/rfc7230#section-1.2 HTTP message headers>.
 
-Nowadays instead of ASCII we typically work with text using an encoding such as UTF-8 that can represent the entire Unicode character set, which includes over a hundred thousand characters and is not limited to the symbols of any particular writing system or culture. However, ASCII is still relevant to network protocols; for example, we can see it in the specification of [HTTP message headers](https://tools.ietf.org/html/rfc7230#section-1.2).
+There is a convenient relationship between ASCII and Unicode: the ASCII
+characters are the first 128 characters of the much larger Unicode character set.
+The <https://www.unicode.org/charts/PDF/U0000.pdf C0 Controls and Basic Latin>
+section of the Unicode standard contains a list of all the ASCII characters.
+You may also find this list replicated in the "ASCII.Char" module; each ASCII
+character corresponds to a constructor of the 'ASCII.Char' type.
 
-There is a convenient relationship between ASCII and Unicode: the ASCII characters are the first 128 characters of the much larger Unicode character set. The [C0 Controls and Basic Latin](https://www.unicode.org/charts/PDF/U0000.pdf) section of the Unicode standard contains a list of all the ASCII characters. You may also find this list replicated in the "ASCII.Char" module; each ASCII character corresponds to a constructor of the 'ASCII.Char' type.
-
-We do not elaborate on the semantics of the control characters here, because this information is both obsolete and restricted by copyright law. It is described by a document entitled /"Coded Character Sets - 7-Bit American National Standard Code for Information Interchange (7-Bit ASCII)"/, published by American National Standards Institute (ANSI) and available for purchase [on their website](https://webstore.ansi.org/Standards/INCITS/INCITS1986R2012).
-
+We do not elaborate on the semantics of the control characters here,
+because this information is both obsolete and restricted by copyright law.
+It is described by a document entitled
+/"Coded Character Sets - 7-Bit American National Standard Code for Information Interchange (7-Bit ASCII)"/,
+published by American National Standards Institute (ANSI) and available for purchase
+<https://webstore.ansi.org/Standards/INCITS/INCITS1986R2012 on their website>.
 -}
-
 module ASCII
   (
     {- * @Char@ -}
